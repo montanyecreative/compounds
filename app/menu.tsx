4035c945e8b11956b99f4a302ec5e734a593c28c
John Montanye <43389272+gatewayitconsulting@users.noreply.github.com>
2023-07-30 23:16:45 -0500

    Nav Bar Mock Clean Up

--- app/menu.tsx
+++ app/menu.tsx
@@ -1,54 +1,24 @@
 export default function Menu() {
 	return (
-		<nav className="flex justify-center">
-			<div className="app-bar flex text-center">
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Home</h2>
+		<nav className="flex justify-center app-bar border-t-2 border-l-2 border-r-2 border-slate-500">
+			<div className="flex text-center">
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Home</h2>
 				</a>
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Brews</h2>
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Brews</h2>
 				</a>
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Beans</h2>
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Beans</h2>
 				</a>
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Methods</h2>
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Methods</h2>
 				</a>
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Grinders</h2>
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Grinders</h2>
 				</a>
-				<a
-					href="#"
-					className="group px-5 py-4 transition-colors hover:opacity-75 hover:dark:border-neutral-700"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl`}>Roasts</h2>
+				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2 className={`text-2xl`}>Roasts</h2>
 				</a>
 			</div>
 		</nav>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Menu from "./menu";
 export default function Home() {
 	return (
 		<main className="flex justify-center">
+			<div className="">
+				<p>hello</p>
+			</div>
 			<Menu />
 		</main>
 	);
