f96b1f13db1e292b1223167b3d0cb7c4136c82f8
John Montanye <43389272+gatewayitconsulting@users.noreply.github.com>
2023-08-12 00:13:21 -0500

    Home Page Mock

--- app/menu.tsx
+++ app/menu.tsx
@@ -1,24 +1,24 @@
 export default function Menu() {
 	return (
-		<nav className="flex justify-center app-bar border-t-2 border-l-2 border-r-2 border-slate-500">
-			<div className="flex text-center">
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Home</h2>
+		<nav className="w-full app-bar-navigation border-t-2 border-l-2 border-r-2 border-slate-500">
+			<div className="flex justify-center text-center">
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Home</h2>
 				</a>
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Brews</h2>
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Brews</h2>
 				</a>
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Beans</h2>
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Beans</h2>
 				</a>
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Methods</h2>
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Methods</h2>
 				</a>
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Grinders</h2>
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Grinders</h2>
 				</a>
-				<a href="#" className="group px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
-					<h2 className={`text-2xl`}>Roasts</h2>
+				<a href="#" className="px-1 lg:px-5 py-4 transition-colors hover:opacity-50 hover:dark:border-neutral-700">
+					<h2>Roasts</h2>
 				</a>
 			</div>
 		</nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,222 @@
 import Menu from "./menu";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
 	return (
-		<main className="flex justify-center">
-			<div className="">
-				<p>hello</p>
+		<main className="flex" style={{ backgroundColor: "#F3F3F3" }}>
+			<div className="mx-3 w-full">
+				<h1>Brews</h1>
+				<div className="brews">
+					<Card className="my-3">
+						<CardHeader>
+							<CardTitle>Coffee Name</CardTitle>
+						</CardHeader>
+						<Separator style={{ marginBottom: "5px" }} />
+						<CardContent className="grid grid-cols-4 gap-3">
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Region</p>
+								<p>Region Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast</p>
+								<p>Roast Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast Date</p>
+								<p>Roast Date Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Process</p>
+								<p>Process Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Method</p>
+								<p>Brew Method Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Grind Setting</p>
+								<p>Grind Setting Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water Temp</p>
+								<p>Water Temp Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Dose</p>
+								<p>Dose Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water In</p>
+								<p>Water In Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Time</p>
+								<p>Brew Time Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Notes</p>
+								<p>Notes Test Data</p>
+							</div>
+						</CardContent>
+					</Card>
+					<Card className="my-3">
+						<CardHeader>
+							<CardTitle>Coffee Name</CardTitle>
+						</CardHeader>
+						<Separator style={{ marginBottom: "5px" }} />
+						<CardContent className="grid grid-cols-4 gap-3">
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Region</p>
+								<p>Region Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast</p>
+								<p>Roast Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast Date</p>
+								<p>Roast Date Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Process</p>
+								<p>Process Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Method</p>
+								<p>Brew Method Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Grind Setting</p>
+								<p>Grind Setting Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water Temp</p>
+								<p>Water Temp Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Dose</p>
+								<p>Dose Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water In</p>
+								<p>Water In Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Time</p>
+								<p>Brew Time Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Notes</p>
+								<p>Notes Test Data</p>
+							</div>
+						</CardContent>
+					</Card>
+					<Card className="my-3">
+						<CardHeader>
+							<CardTitle>Coffee Name</CardTitle>
+						</CardHeader>
+						<Separator style={{ marginBottom: "5px" }} />
+						<CardContent className="grid grid-cols-4 gap-3">
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Region</p>
+								<p>Region Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast</p>
+								<p>Roast Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast Date</p>
+								<p>Roast Date Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Process</p>
+								<p>Process Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Method</p>
+								<p>Brew Method Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p>Grind Setting</p>
+								<p>Grind Setting Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water Temp</p>
+								<p>Water Temp Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Dose</p>
+								<p>Dose Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water In</p>
+								<p>Water In Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Time</p>
+								<p>Brew Time Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Notes</p>
+								<p>Notes Test Data</p>
+							</div>
+						</CardContent>
+					</Card>
+					<Card className="my-3">
+						<CardHeader>
+							<CardTitle>Coffee Name</CardTitle>
+						</CardHeader>
+						<Separator style={{ marginBottom: "5px" }} />
+						<CardContent className="grid grid-cols-4 gap-3">
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Region</p>
+								<p>Region Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast</p>
+								<p>Roast Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Roast Date</p>
+								<p>Roast Date Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Process</p>
+								<p>Process Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Method</p>
+								<p>Brew Method Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Grind Setting</p>
+								<p>Grind Setting Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water Temp</p>
+								<p>Water Temp Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Dose</p>
+								<p>Dose Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Water In</p>
+								<p>Water In Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Brew Time</p>
+								<p>Brew Time Test Data</p>
+							</div>
+							<div className="grid grid-cols-1">
+								<p className="font-bold">Notes</p>
+								<p>Notes Test Data</p>
+							</div>
+						</CardContent>
+					</Card>
+				</div>
 			</div>
 			<Menu />
 		</main>
