58d6cf41fadf9cebca4807b5c888f534c0682a8d
John Montanye <43389272+gatewayitconsulting@users.noreply.github.com>
2023-08-12 13:57:43 -0500

    Implement Basic Add Brew Dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Menu from "./menu";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
@@ -10,15 +14,97 @@ export default function Home() {
 				<h1 className="text-3xl mt-5">Brews</h1>
 				<div className="brews-add" style={{ position: "absolute", top: "30px", right: "20px" }}>
 					<Dialog>
-						<DialogTrigger>Add</DialogTrigger>
-						<DialogContent>
+						<DialogTrigger asChild>
+							<Button variant="outline">Add</Button>
+						</DialogTrigger>
+						<DialogContent className="sm:max-w-[425px] lg:max-w-[1000px]">
 							<DialogHeader>
-								<DialogTitle>Are you sure absolutely sure?</DialogTitle>
-								<DialogDescription>
-									This action cannot be undone. This will permanently delete your account and remove your data from our
-									servers.
-								</DialogDescription>
+								<DialogTitle>Add Brew</DialogTitle>
+								<DialogDescription>Add details for your brew here.</DialogDescription>
 							</DialogHeader>
+							<div className="grid gap-4 py-4">
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="coffeeName" className="text-right">
+										Coffee Name
+									</Label>
+									<Input id="coffeeName" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="region" className="text-right">
+										Region
+									</Label>
+									<Input id="region" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="roast" className="text-right">
+										Roast
+									</Label>
+									<Input id="roast" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="roastDate" className="text-right">
+										Roast Date
+									</Label>
+									<Input id="roastDate" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="region" className="text-right">
+										Region
+									</Label>
+									<Input id="region" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="process" className="text-right">
+										Process
+									</Label>
+									<Input id="process" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="brewMethod" className="text-right">
+										Brew Method
+									</Label>
+									<Input id="brewMethod" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="grindSetting" className="text-right">
+										Grind Setting
+									</Label>
+									<Input id="grindSetting" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="waterTemp" className="text-right">
+										Water Temp
+									</Label>
+									<Input id="waterTemp" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="dose" className="text-right">
+										Dose
+									</Label>
+									<Input id="dose" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="waterIn" className="text-right">
+										Water In
+									</Label>
+									<Input id="waterIn" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="brewTime" className="text-right">
+										Brew Time
+									</Label>
+									<Input id="brewTime" value="" className="col-span-3" />
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="notes" className="text-right">
+										Notes
+									</Label>
+									<Input id="notes" value="" className="col-span-3" />
+								</div>
+							</div>
+							<DialogFooter>
+								<Button type="submit">Add brew</Button>
+							</DialogFooter>
 						</DialogContent>
 					</Dialog>
 				</div>
